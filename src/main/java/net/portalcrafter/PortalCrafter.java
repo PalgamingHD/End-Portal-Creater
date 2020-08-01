40e82170ffbd84f7b9e0b66fd3b2e168fca69fd5
PalgamingHD <49602450+PalgamingHD@users.noreply.github.com>
2020-08-01 00:34:48 -0500

    Add files via upload

---
+++ src/main/java/net/portalcrafter/PortalCrafter.java
@@ -0,0 +1,12 @@
+package net.portalcrafter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PortalCrafter implements ModInitializer
+{
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
